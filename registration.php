40877788b48799ae641921e5302170150a6e4e24
Rizvee-Ahmed <mayinuddina@gmail.com>
2021-05-31 20:23:32 +0600

    first commit

---
+++ registration.php
@@ -0,0 +1,77 @@
+
+    <!DOCTYPE html>
+<html>
+    <head>
+        <title>Registration Form</title>
+</head>
+        <body>
+                <h1><u> Registration Form</u></h1>
+<form>
+    <table>
+        <tr>
+            <td>
+                Full Name:
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>  
+        <tr>
+            <td>
+                Email: 
+            </td>
+            <td>
+                <input type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Password: 
+            </td>
+            <td>
+                <input type="password">
+            </td>
+        <tr>
+            
+           
+        </tr>
+        <td>
+                Comment:
+            </td>
+            <td>
+                <textarea name="" id="" cols="30" rows="4">
+                </textarea>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+             
+        </tr>
+       
+        <tr>
+              <td> 
+                   Gender: 
+              </td>
+               <td>   
+                        <input type="radio" name= "gender">Female
+                        <input type="radio" name="gender">Male
+                        <input type="radio" name="gender">Other
+               </td>
+               
+        </tr>
+        <tr>
+              <td>  
+                   Please choose a hobby:
+                </td>
+                <td> 
+                        <input type="radio" name= "hobby">Singing
+                        <input type="radio" name="hobby">Dancing
+                        <input type="radio" name="hobby">Reading
+                </td>
+                
+        </tr>
+        
+    </table>
+        </form>
+    </body>
+</html>
